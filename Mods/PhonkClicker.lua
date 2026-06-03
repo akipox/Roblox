495b1aaa5b7393d200c76b9c084864984c2f689f
akipox38 <120580021+akipox38@users.noreply.github.com>
2026-06-03 23:24:41 +0700

    v2

--- Mods/PhonkClicker.lua
+++ Mods/PhonkClicker.lua
@@ -1,5 +1,6 @@
 local SimpleUI=loadstring(game:HttpGet("http://raw.githubusercontent.com/PaazlisMaswa/RobloxProject/refs/heads/main/Packages/SimpleUI/init.luau"))()
 
+local ReplicatedStorage=game:GetService("ReplicatedStorage")
 local RunService=game:GetService("RunServie")
 
 local Window = SimpleUI:CreateWindow({
@@ -7,21 +8,23 @@ local Window = SimpleUI:CreateWindow({
 })
 
 local AutoClickEnabled=false
+local ClickBrainrotRE=nil
 
 Window:CreateToggle({Name = "Auto Clicker", CurrentValue = false, Callback=function(value)
    if value then
       if AutoClickEnabled then return end
       AutoClickEnabled=true
-      task.spawn()
+      task.spawn(function()
          while AutoClickEnabled then
             task.wait()
-            
+			if not ClickBrainrotRE or not ClickBrainrotRE.Parent then
+				ClickBrainrotRE=ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("ClickBrainrot")
+			end
+            ClickBrainrotRE:FireServer(1)
          end
-      end
+      end)
    end
 end})
 
 
 local Credit = Window:AddLabel({Text = "YouTube: None"})
-  
-    
