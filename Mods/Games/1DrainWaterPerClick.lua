a79f7d903b42591acb775095a4f2bb2e86f4924e
akipox <120580021+akipox@users.noreply.github.com>
2026-08-18 07:47:50 +0700

    +1 Drain Water Per Click Template 08-15-2026

--- Mods/Games/1DrainWaterPerClick.lua
+++ Mods/Games/1DrainWaterPerClick.lua
@@ -456,6 +456,7 @@ Window:AddToggle({
 	Value = false,
 	Flag = "click_enabled",
 	Callback = function(value)
+		value = false
 		Enableds.Click = value
 		HandleClick()
 	end
@@ -477,6 +478,7 @@ HitToggle = Window:AddToggle({
 	Value = false,
 	Flag = "hit_enabled",
 	Callback = function(value)
+		value = false
 		Enableds.Hit = value
 		HandleHit()
 	end
@@ -487,6 +489,7 @@ CashToggle = Window:AddToggle({
 	Value = false,
 	Flag = "cash_enabled",
 	Callback = function(value)
+		value = false
 		Enableds.Cash = value
 		HandleCash()
 	end
@@ -497,6 +500,7 @@ Window:AddToggle({
 	Value = false,
 	Flag = "equip_best_fish_enabled",
 	Callback = function(value)
+        value = false
 		Enableds.EquipBestFish = value
 		HandleEquipBestFish()
 	end
@@ -507,16 +511,21 @@ Window:AddToggle({
 	Value = false,
 	Flag = "rebirth_enabled",
 	Callback = function(value)
+		value = false
 		Enableds.Rebirth = value
 		HandleRebirth()
 	end
 })
+
+Window:AddLabel({ Text = "+ More Feature", TextColor3 = Color3.fromRGB(255, 255, 255) })
+if true then return end
 
 Window:AddToggle({
 	Text = "Auto Sell",
 	Value = false,
 	Flag = "sell_enabled",
 	Callback = function(value)
+        value = false
 		Enableds.Sell = value
 		HandleSell()
 	end
@@ -541,6 +550,7 @@ Window:AddToggle({
 	Value = false,
 	Flag = "upgrade_enabled",
 	Callback = function(value)
+		value = false
 		Enableds.Upgrade = value
 		HandleUpgrade()
 	end
