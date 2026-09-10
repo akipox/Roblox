94b8ac2bbbde24a9590c33e83c6b94362a9007c5
akipox <120580021+akipox@users.noreply.github.com>
2026-09-10 18:41:12 +0700

    Steal an Egg 08-26-2026

--- Mods/Games/StealanEgg.lua
+++ Mods/Games/StealanEgg.lua
@@ -1,7 +1,6 @@
 local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/main/Packages/Sampluy/init.luau"))()
 
 local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
-
 local Players = Services.Players
 local RunService = Services.RunService
 local ReplicatedStorage = Services.ReplicatedStorage
