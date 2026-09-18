46d5647667faac39e492c6bac5320a02401ec3b5
akipox <120580021+akipox@users.noreply.github.com>
2026-09-19 06:06:16 +0700

    Murder Mystery 2

---
+++ Mods/Games/MurderMystery2.lua
@@ -0,0 +1,254 @@
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/main/Packages/Sampluy/init.luau"))()
+local Observe = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crokier/Roblox/main/Packages/Observe/init.luau"))()
+
+local Services = setmetatable({}, {__index = function(_, i) return cloneref and cloneref(game:GetService(i)) or game:GetService(i) end})
+
+local Enableds, Connections = {["Chameleon"] = false}, {}
+local ChameleonsFolder = workspace:FindFirstChild("Characters")
+local BillboardPool = {}
+local ActiveESP = {}
+local CharacterActives={}
+
+local CleanPlayer = Observe.ObservePlayer(function(player)
+    local cleanCharacter = Observe.ObserveCharacter(player, function(character)
+        CharacterActives[character]=true
+        return function()
+           CharacterActives[character]=nil
+        end
+    end)
+    return function()
+       cleanCharacter()
+    end
+end)
+
+local function WaitForChildOfClass(parent, className, timeout)
+	timeout = typeof(timeout) == "number" and timeout or 5
+	
+	local existingChild = parent:FindFirstChildOfClass(className)
+	if existingChild then 
+		return existingChild 
+	end
+
+	local currentThread = coroutine.running()
+	local connection = nil
+	local timeoutThread = nil
+
+	connection = parent.ChildAdded:Connect(function(child)
+		if child:IsA(className) then
+			if connection then connection:Disconnect() connection = nil end
+			if timeoutThread and coroutine.status(timeoutThread) ~= "dead" then task.cancel(timeoutThread) timeoutThread = nil end
+			task.spawn(currentThread, child)
+		end
+	end)
+
+	timeoutThread = task.delay(timeout, function()
+		if connection and connection.Connected then
+			connection:Disconnect()
+			connection= nil
+			warn(string.format("Infinite yield possible on '%s:WaitForChildOfClass(\"%s\")'", parent:GetFullName(), className))
+			task.spawn(currentThread, nil)
+			currentThread = nil
+		end
+	end)
+	
+	return coroutine.yield()
+end
+
+local function GetBillboard()
+	local billboard = table.remove(BillboardPool)
+
+	if not billboard then
+		billboard = Instance.new("BillboardGui")
+		billboard.Name = math.random()
+		billboard.Size = UDim2.new(0, 150, 0, 30)
+		billboard.AlwaysOnTop = true
+		billboard.StudsOffset = Vector3.new(0, 2, 0)
+
+		local label = Instance.new("TextLabel")
+		label.Name = "Title"
+		label.Size = UDim2.new(1, 0, 1, 0)
+		label.BackgroundTransparency = 1
+		label.TextColor3 = Color3.fromRGB(255, 30, 30)
+		label.TextSize = 14
+		label.Font = Enum.Font.SourceSansBold
+		label.TextStrokeTransparency = 0
+		label.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
+		label.Parent = billboard
+	end
+
+	billboard.Enabled = true
+	return billboard
+end
+
+local function ReleaseBillboard(billboard)
+	billboard.Enabled = false
+	billboard.Adornee = nil
+	table.insert(BillboardPool, billboard)
+end
+
+local function RemoveESP(child)
+	local data = ActiveESP[child]
+	if data then
+		ReleaseBillboard(data.Billboard)
+	end
+end
+
+local function DestroyESP(child)
+	local data = ActiveESP[child]
+	if data then
+		ActiveESP[child] = nil
+
+		if data.Connections then
+			for _, connection in pairs(data.Connections) do
+				connection:Disconnect()
+			end
+		end
+
+		ReleaseBillboard(data.Billboard)
+	end
+end
+
+local function SetESP(child, data)
+    local billboard = data.Billboard
+
+	local adornee = child
+    billboard.Adornee = adornee
+	billboard.Enabled = true
+
+	local label = billboard:FindFirstChild("Title")
+	label.Text = child.Name
+	
+	local billboardGui = WaitForChildOfClass(child, "BillboardGui", 5)
+	
+	if billboardGui then
+	   label.TextColor3 = Color3.fromRGB(255, 0, 0)
+	end
+end
+
+local function AddESP(child)
+	-- Cegah duplikasi atau jika toggle dimatikan di tengah jalan
+	if ActiveESP[child] ~= nil or not Enableds.Chameleon then return end
+  local attempt, knife = 0, nil
+  repeat
+      for i,v in ipairs(child:GetChildren()) do
+         if v and v.Parent and v:IsA("Tool") and v.Name=='Knife' then
+            knife=v
+            break
+         end
+      end
+      attempt+= 1
+      task.wait(1)
+  until knife ~= nil and attempt>=10 or not child.Parent
+  
+	local adornee = child
+	if not (adornee and adornee.Parent) then return end
+
+	local data = {}
+	
+	local billboard = GetBillboard()
+	billboard.Adornee = adornee
+	billboard.Parent = ParentGui
+	data.Billboard = billboard
+	
+	local label = billboard:FindFirstChild("Title")
+	label.Text = child.Name
+
+	local billboardGui = WaitForChildOfClass(child, "BillboardGui", 5)
+	if not billboardGui then return end
+
+	local childConnections = {}
+
+	label.TextColor3 = billboardGui.Enabled == true and Color3.fromRGB(255, 255, 255) or Color3.fromRGB(255, 30, 30)
+
+	childConnections.BillboardGuiChanged = billboardGui:GetPropertyChangedSignal("Enabled"):Connect(function(_, parent)
+		label.TextColor3 = billboardGui.Enabled == true and Color3.fromRGB(255, 255, 255) or Color3.fromRGB(255, 30, 30)
+	end)
+
+	childConnections.AncestryChanged = child.AncestryChanged:Connect(function(_, parent)
+		if not parent or not child:IsDescendantOf(workspace) and childConnections.AncestryChanged.Connected then
+			DestroyESP(child)
+		end
+	end)
+
+  childConnections.KnifeAncestryChanged = knife.AncestryChanged:Connect(function(_, parent)
+		if not parent or not child:IsDescendantOf(child) then
+      childConnections.AncestryChanged:Disconnect()
+			DestroyESP(child)
+		end
+	end)
+	data.Connections = childConnections
+	ActiveESP[child] = data
+end
+
+local function ProcessChameleon(child)
+	if ActiveESP[child] or not Enableds.Chameleon then return end
+	if not (child and child.Parent) then return end
+	AddESP(child)
+end
+
+local function RemoveAllESP()
+	for child, _ in pairs(ActiveESP) do
+		RemoveESP(child)
+	end
+end
+
+local function ClearAllESP()
+	for child, _ in pairs(ActiveESP) do
+		DestroyESP(child)
+	end
+end
+
+local function ESPChameleon()
+	if Connections.ChameleonAdded then Connections.ChameleonAdded:Disconnect() Connections.ChameleonAdded = nil end
+	RemoveAllESP()
+	if not Enableds.Chameleon then return end
+	
+	if next(ActiveESP) then
+		for child, data in pairs(ActiveESP) do
+			if not Enableds.Chameleon then break end
+			SetESP(child, data)
+	    end
+	end
+	
+	task.spawn(function()
+		while Enableds.Chameleon do
+			for character, enabled in pairs(CharacterActives) do
+				if not Enableds.Chameleon then break end
+				ProcessChameleon(character)
+				task.wait()
+			end
+			task.wait(1)
+		end
+	end)
+end
+
+local Window = UI:CreateWindow({
+	Name = "Murder Mystery 2",
+	Destroying = function()
+		for key, enabled in pairs(Enableds) do
+			Enableds[key] = false
+		end
+		for key, connection in pairs(Connections) do
+			if connection then
+				connection:Disconnect()
+			end
+		end
+		ClearAllESP()
+	end
+})
+
+ParentGui = Window.Gui
+
+Window:AddToggle({
+	Text = "ESP Murder", 
+	Value = false, 
+	Callback = function(value)
+		Enableds.Chameleon = value
+		ESPChameleon()
+	end
+})
+
+Window:AddLabel({
+	Text = "YouTube: Crokyreo",
+	TextColor3 = Color3.fromRGB(255, 255, 255)
+})
