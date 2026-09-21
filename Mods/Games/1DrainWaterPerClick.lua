d5f591dffa174639487f52197fb3dee49942b8e8
akipox <120580021+akipox@users.noreply.github.com>
2026-09-22 04:57:45 +0700

    +1 Drain Water Per Click 08-15-2026

--- Mods/Games/1DrainWaterPerClick.lua
+++ Mods/Games/1DrainWaterPerClick.lua
@@ -250,7 +250,7 @@ local Plot = GetPlot()
 
 local Window = UI:CreateWindow({
 	Name = "+1 Drain Water Per Click",
-    ConfigInfo = {Enabled=true,Path="Crokyreo/Target/configs.json"},
+    ConfigInfo = {Enabled=true,Path="Crokyreo/1DrainWaterPerClick/configs.json"},
 	Destroying = function()
 		for key, enabled in pairs(Enableds) do
 			Enableds[key] = false
